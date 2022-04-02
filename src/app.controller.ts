9242364985de98a4bd0d0a33e41169126c3f2024
Pulkit Sachdeva <hrtsachdeva@gmail.com>
2022-04-02 16:58:47 +0530

    Added swagger configurations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {ApiResponse, ApiTags, ApiOperation} from '@nestjs/swagger'
 
+@ApiTags('App Controller')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary:'Dummy API',
+    description:'This api is used to check the connection'
+  })
+  @ApiResponse({
+    status:201,
+    description: 'Connection Successfult',
+    // type:any
+  })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('NestJs BoilerPlate')
+    .setDescription('BolierPlate code for nestJS')
+    .setVersion('1.0')
+    .addTag('BoilerPlate')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
